0fe64e5846cf5c7fb966354b73a6fec3260aafa5
Sean Siggard <sean.siggard@hq.bill.com>
2024-09-06 11:32:55 -0400

    first commit

---
+++ Squads/SquadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquadsApp.swift
+//  Squads
+//
+//  Created by Sean Siggard on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SquadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
